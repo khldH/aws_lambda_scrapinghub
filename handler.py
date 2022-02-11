e3442e002c5bbfe4eacfa14d836c49be8982bde9
khalidh <khalidh@directferries.com>
2022-02-11 10:26:00 +0000

    refactor

--- handler.py
+++ handler.py
@@ -2,20 +2,22 @@ import json
 import os
 from scrapinghub import ScrapinghubClient
 
+print('here')
+scraping_hub_key = os.environ['scraping_hub_key']
+client = ScrapinghubClient(scraping_hub_key)
+project = client.get_project(494606)
+spider = project.spiders.get('job')
+spider.jobs.run(job_settings={'DELTAFETCH_ENABLED': 'False'})
+print(scraping_hub_key)
+print(project.spiders)
 
-def lambda_handler(event, context):
+# def lambda_handler():
+#     scraping_hub_key = os.environ['scraping_hub_key']
+#     client = ScrapinghubClient(scraping_hub_key)
+#     project = client.get_project(494606)
+#     spider = project.spiders.get('job')
+#     spider.jobs.run(job_settings={'DELTAFETCH_ENABLED': 'False'})
+#     print(scraping_hub_key)
+#     print(project.spiders)
 
-    scraping_hub_key = os.environ['scraping_hub_key']
-    client = ScrapinghubClient(scraping_hub_key)
-    project = client.get_project(494606)
-    spider = project.spiders.get('job')
-    spider.jobs.run(job_settings={'DELTAFETCH_ENABLED': 'False'})
-    print(scraping_hub_key)
-    print(project.spiders)
-
-    body = {
-        "message": "success!",
-        "input": event,
-    }
-
-    return {"statusCode": 200, "body": json.dumps(body)}
+    # return {"statusCode": 200, "body": json.dumps(body)}
