e680df743339b831572177daaa5e413e1bf7aa43
khalidh <khalidh@directferries.com>
2022-02-11 11:00:05 +0000

    refactor

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import os
 from scrapinghub import ScrapinghubClient
 
 print('here')
-scraping_hub_key = os.environ['scraping_hub_key']
+scraping_hub_key = os.environ['SCRAPING_HUB_KEY']
 client = ScrapinghubClient(scraping_hub_key)
 project = client.get_project(494606)
 spider = project.spiders.get('job')
